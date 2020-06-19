0645a2f642a6d13a9433ed2709b78eddad26de1c
Dmitry <dmitryorly611@gmail.com>
2020-06-19 22:25:20 +0300

    glide.js mount with some options

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 import '../sass/main.sass'
 import modals from './modal.js'
 import headerMenu from './header.js'
+import Glide from '@glidejs/glide'
 
 headerMenu()
 modals()
+
+let slider = new Glide('.glide', {
+  perView: 4,
+  gap: 20,
+  type: 'carousel'
+})
+slider.mount()
