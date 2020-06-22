b5c5a64076d548cdf685ad4efdfd6e885f3d5f46
Dmitry <dmitryorly611@gmail.com>
2020-06-22 21:39:05 +0300

    call .mount() for glide.js in catalog

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,31 @@ import Glide from '@glidejs/glide'
 headerMenu()
 modals()
 
-let slider = new Glide('.glide', {
-  perView: 4,
-  gap: 20,
-  type: 'carousel'
-})
-slider.mount()
+
+try {
+  let gallery = new Glide('.glide', {
+    perView: 4,
+    gap: 20,
+    type: 'carousel'
+  })
+  gallery.mount()
+} catch (e) {
+  console.log(e);
+}
+
+try {
+  let slider = new Glide('.catalog__slider', {
+    perView: 4,
+    type: 'carousel',
+    gap: 40,
+    breakpoints: {
+      1340: {perView: 2},
+      425: {perView: 1}
+    }
+  })
+  slider.mount()
+} catch (e) {
+  console.log(e);
+}
+
+
